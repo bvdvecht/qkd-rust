4e003bb666a49a56258c7b129c995b7312457270
Bart van der Vecht <bartvdv@outlook.com>
2020-01-09 13:17:20 +0100

    Refactoring

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
-use std::os::raw::c_char;
+#![allow(non_snake_case)]
+#![allow(unused_variables)]
 
+use std::os::raw::c_char;
 use std::sync::mpsc;
 use mpsc::Sender;
 use mpsc::Receiver;
@@ -22,31 +24,32 @@ pub struct QoS {
 }
 
 
+// QKD API
+
 #[no_mangle]
-#[allow(non_snake_case)]
-#[allow(unused_variables)]
 pub extern fn QKD_open(destination: *mut c_char, qos: QoS, key_handle_ptr: *mut u8) -> u32 {
     println!("QKD_OPEN");
 
     // ignore destination for now
 
-    session::set_key_length(qos.requested_length as usize);
     println!("qos req lengt: {}", qos.requested_length);
+    println!("key_handle: {:x?}", key_handle_ptr);
 
     let key_handle = KeyHandle::from(key_handle_ptr);
-    println!("key_handle: {:x?}", key_handle_ptr);
     key_handle.print();
 
+    // create channels for communication between this and qkd threads
+    let (tx, rx): (Sender<Key>, Receiver<Key>) = mpsc::channel();
+
     if key_handle.is_null() {
-        session::set_is_server(true);
         key_handle.set_value();
+        session::create_qkd_session(true, qos.requested_length as usize, key_handle, rx);
+    } else {
+        session::create_qkd_session(false, qos.requested_length as usize, key_handle, rx)
     }
 
-    let (tx, rx): (Sender<Key>, Receiver<Key>) = mpsc::channel();
-    session::set_rx(rx);
-
     println!("spawning qkd generator...");
-    quantum::spawn_qkd_generator(tx);
+    quantum::spawn_qkd_thread(tx);
     println!("spawned");
 
     println!("QKD_open end");
@@ -55,33 +58,27 @@ pub extern fn QKD_open(destination: *mut c_char, qos: QoS, key_handle_ptr: *mut 
 }
 
 #[no_mangle]
-#[allow(non_snake_case)]
-#[allow(unused_variables)]
 pub extern fn QKD_connect_nonblock(key_handle: *mut u8) -> u32 {
     println!("QKD_CONNECT_NONBLOCK");
-
     0
 }
 
 #[no_mangle]
-#[allow(non_snake_case)]
-#[allow(unused_variables)]
 pub extern fn QKD_connect_blocking(key_handle: *mut u8, timeout: u32) -> u32 {
     println!("QKD_CONNECT_BLOCKING");
-
     0
 }
 
 
 #[no_mangle]
-#[allow(non_snake_case)]
-#[allow(unused_variables)]
 pub extern fn QKD_get_key(key_handle: *mut u8, key_buffer: *mut u8) -> u32 {
     println!("QKD_GET_KEY");
 
+    // wait for key from qkd thread
     let key = session::get_rx().recv().unwrap();
     println!("key: {:?}", key.to_string());
     
+    // copy to buffer
     key.write_to_buffer(key_buffer);
 
     println!("QKD_get_key end");
@@ -90,17 +87,12 @@ pub extern fn QKD_get_key(key_handle: *mut u8, key_buffer: *mut u8) -> u32 {
 }
 
 #[no_mangle]
-#[allow(non_snake_case)]
-#[allow(unused_variables)]
 pub extern fn QKD_close(key_handle: *mut u8) -> u32 {
     println!("QKD_CLOSE");
-
     0
 }
 
 #[no_mangle]
-#[allow(non_snake_case)]
-#[allow(unused_variables)]
 pub extern fn QKD_init(am_server: bool) -> u32 {
     0
 }
--- src/session.rs
+++ src/session.rs
@@ -4,70 +4,56 @@ use std::sync::mpsc;
 use mpsc::Receiver;
 
 use crate::quantum::Key;
+use crate::keyhandle::KeyHandle;
 
-static mut QKD_SESSION: QkdSession = QkdSession { is_server: false, key_length: 0, key_handle: [5; 64], rx: None };
+// singleton QkdSession
+static mut QKD_SESSION: Option<QkdSession> = None;
 
 pub struct QkdSession {
     is_server: bool,
     key_length: usize,
-    key_handle: [u8; 64],
-    rx: Option<Receiver<Key>>
+    key_handle: KeyHandle,
+    rx: Receiver<Key>
 }
 
-fn get_static_qkd_session() -> &'static mut QkdSession {
-    unsafe { &mut QKD_SESSION }
+fn get_qkd_session() -> &'static mut QkdSession {
+    unsafe {
+        if let Some(session) = &mut QKD_SESSION {
+            session
+        } else {
+            panic!("trying to get session but it does not exist");
+        }
+    }
 }
 
-pub fn set_is_server(b: bool) {
-    get_static_qkd_session().set_is_server(b);
+pub fn create_qkd_session(is_server: bool, key_length: usize, key_handle: KeyHandle, rx: Receiver<Key>) {
+    unsafe {
+        QKD_SESSION = Some(QkdSession {is_server, key_length, key_handle, rx});
+    }
 }
 
 pub fn get_is_server() -> bool {
-    get_static_qkd_session().get_is_server()
-}
-
-pub fn set_key_length(len: usize) {
-    get_static_qkd_session().set_key_length(len);
+    get_qkd_session().get_is_server()
 }
 
 pub fn get_key_length() -> usize {
-    get_static_qkd_session().get_key_length()
-}
-
-pub fn set_rx(rx: Receiver<Key>) {
-    get_static_qkd_session().set_rx(rx);
+    get_qkd_session().get_key_length()
 }
 
 pub fn get_rx() -> &'static Receiver<Key> {
-    get_static_qkd_session().get_rx()
+    get_qkd_session().get_rx()
 }
 
 impl QkdSession {
-    fn set_is_server(&mut self, b: bool) {
-        self.is_server = b;
-    }
-
     fn get_is_server(&self) -> bool {
         self.is_server
-    }
-
-    fn set_key_length(&mut self, len: usize) {
-        self.key_length = len;
     }
 
     fn get_key_length(&self) -> usize {
         self.key_length
     }
 
-    fn set_rx(&mut self, rx: Receiver<Key>) {
-        self.rx = Some(rx);
-    }
-
     fn get_rx(&self) -> &Receiver<Key> {
-        if let Some (rx) = &self.rx {
-            rx
-        } else {
-            panic!("rx is None");
-        }
+        &self.rx
     }
 }
